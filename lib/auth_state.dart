24d6659fd172c2da323a09d394171019261516b5
genie0806 <genie0806@gmail.com>
2021-10-20 11:59:37 +0900

    하단 클릭 이벤트 환경에 맞춰서 문구 변경

--- lib/auth_state.dart
+++ lib/auth_state.dart
@@ -30,18 +30,30 @@ class _AuthStateState extends State<AuthState>
               child: Container(
                 color: Colors.white,
                 child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      if (selectedForm == 0) {
-                        selectedForm = 1;
-                      } else {
-                        selectedForm = 0;
-                      }
-                    });
-                  },
-                  child: const Text('sign Up',
-                      style: TextStyle(color: Colors.black87)),
-                ),
+                    onPressed: () {
+                      setState(() {
+                        if (selectedForm == 0) {
+                          selectedForm = 1;
+                        } else {
+                          selectedForm = 0;
+                        }
+                      });
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                          text: (selectedForm == 0)
+                              ? 'Already have an account?'
+                              : "Don't have an account",
+                          style: const TextStyle(color: Colors.green),
+                          children: [
+                            TextSpan(
+                                text:
+                                    (selectedForm == 0) ? 'Sign in' : 'Sign up',
+                                style: const TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold)),
+                          ]),
+                    )),
               ),
             ),
           ],
